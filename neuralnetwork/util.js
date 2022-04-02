63db9364624124481e4ace122e60a4b90c694328
Artem Potafiy <apotafiy@uw.edu>
2022-04-02 15:09:35 -0700

    default genome adjustment

--- neuralnetwork/util.js
+++ neuralnetwork/util.js
@@ -1,20 +1,27 @@
 const getDefaultGenome = () => {
     // construct the default genome and return it
 
-    const SENSOR = 'SENSOR';
-    const HIDDEN = 'HIDDEN';
-    const OUTPUT = 'OUTPUT';
+    const nodeTypes = {
+        sensor: 0,
+        hidden: 1,
+        output: 2,
+    };
     const NodeGenes = [
-            { nodeId: 1, type: SENSOR },
-            { nodeId: 2, type: HIDDEN },
-            { nodeId: 3, type: OUTPUT },
-        ];
+        { nodeId: 1, type: nodeTypes.sensor },
+        { nodeId: 2, type: nodeTypes.hidden },
+        { nodeId: 3, type: nodeTypes.output },
+    ];
     const ConnectionGenes = [
-            { in: NodeGenes[0], out: NodeGenes[1], weight: 0.1, enabled: true, innovation: 1 },
-        ];
+        {
+            in: NodeGenes[0],
+            out: NodeGenes[1],
+            weight: 0.1,
+            isEnabled: true,
+            innovation: 1,
+        },
+    ];
     return {
         NodeGenes,
         ConnectionGenes,
     };
 };
-
