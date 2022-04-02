d3dcafdccb81f8042a5fc4dfecf1b1d138983ad1
Artem Potafiy <apotafiy@uw.edu>
2022-04-02 01:54:23 -0700

    add default genome frame

--- neuralnetwork/util.js
+++ neuralnetwork/util.js
@@ -1,5 +1,20 @@
 const getDefaultGenome = () => {
     // construct the default genome and return it
-    //
-    const genome = { NodeGenes: [], ConnectionGenes: [] };
+
+    const SENSOR = 'SENSOR';
+    const HIDDEN = 'HIDDEN';
+    const OUTPUT = 'OUTPUT';
+    const NodeGenes = [
+            { nodeId: 1, type: SENSOR },
+            { nodeId: 2, type: HIDDEN },
+            { nodeId: 3, type: OUTPUT },
+        ];
+    const ConnectionGenes = [
+            { in: NodeGenes[0], out: NodeGenes[1], weight: 0.1, enabled: true, innovation: 1 },
+        ];
+    return {
+        NodeGenes,
+        ConnectionGenes,
+    };
 };
+
