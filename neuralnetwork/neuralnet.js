f2c646d255cf6beb9874aebb412ebf4d05e9d9d7
Parker Rosengreen <parker19@uw.edu>
2022-04-04 19:12:01 -0700

    added topological sort

--- neuralnetwork/neuralnet.js
+++ neuralnetwork/neuralnet.js
@@ -4,23 +4,57 @@ class NeuralNet {
         this.structure = getDefaultGenome();
         this.nodes = this.structure.NodeGenes;
         this.edges = this.structure.ConnectionGenes;
+        this.numInputs = 1;
     };
 
     processInput(input) {
-        let index = 0;
-        this.nodes.forEach(node => {
-            if (node.type === nodeTypes.input) {
-                node.value = input[index]; // consider normalizing on the agent side?
-                index++;
-            } else {
+        let sortedNodes = this.topoSort();
+
+        sortedNodes.forEach(nodeId => {
+            if (nodeId <= this.numInputs) { // assign values to input neurons
+                this.nodes[nodeId - 1] = input[nodeId - 1];
+            } else { // hidden or output neurons
                 let value = 0;
                 this.edges.forEach(edge => {
-                    if (edge.out === node && edge.isEnabled) {
+                    if (edge.out.id === nodeId && edge.isEnabled) {
                         value += edge.in.value * edge.weight;
                     }
                 });
-                node.value = value;
+                this.nodes[nodeId - 1] = value;
             }
         });
+    };
+
+    topoSort() {
+        let inMap = {};
+        let nodeQueue = [];
+        let sortedNodes = [];
+        
+        for (let id = 1; id <= this.nodes.length; id++) { // map neurons to number of incoming edges
+            inMap[id] = 0;
+            this.edges.forEach(edge => {
+                if (edge.out.id === id) {
+                    inMap[id]++;
+                }
+            });
+            if (inMap[id] === 0) {
+                nodeQueue.push[id];
+            }
+        }
+
+        while (nodeQueue.length !== 0) {
+            let id = nodeQueue.splice(0, 1);
+            sortedNodes.push[id];
+            this.edges.forEach(edge => {
+                if (edge.in.id === id) {
+                    inMap[edge.in.id]--;
+                    if (inMap[edge.in.id] === 0) {
+                        nodeQueue.push[edge.in.id];
+                    }
+                }
+            });
+        }
+
+        return sortedNodes;
     };
 };
