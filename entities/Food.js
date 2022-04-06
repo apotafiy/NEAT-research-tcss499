bb1df9bfd8a3173a80cdbeae4e6cff003e85b372
Artem Potafiy <apotafiy@uw.edu>
2022-04-05 21:54:46 -0700

    add food reproduction

--- entities/Food.js
+++ entities/Food.js
@@ -21,14 +21,14 @@ class Food {
                 isSet: false,
             },
             {
-                lifeSpan: 5,
+                lifeSpan: 5 + Math.random(), // add a bit of variation in lifespan 
                 radius: 10,
                 color: 'hsl(110, 100%, 40%)',
                 calories: 10,
                 isSet: false,
             },
             {
-                lifeSpan: 5,
+                lifeSpan: 5, 
                 radius: 15,
                 color: 'hsl(110, 100%, 30%)',
                 calories: 15,
@@ -47,13 +47,38 @@ class Food {
     }
 
     updateBoundingCircle() {
-        this.BC = new BoundingCircle(this.x, this.y, this.properties[this.state].radius);
+        this.BC = new BoundingCircle(
+            this.x,
+            this.y,
+            this.properties[this.state].radius
+        );
     }
 
-    consume(){
+    consume() {
         this.state = this.states.dead;
         // if isPoison then energy is depleted
-        return this.isPoison ? Math.abs(this.properties[this.state].calories) * -1: this.properties[this.state].calories;
+        return this.isPoison
+            ? Math.abs(this.properties[this.state].calories) * -1
+            : this.properties[this.state].calories;
+    }
+
+    reproduce(numChildren) {
+        // determine a circle around food where it reproduce
+        // use the number of children to determine the angle to place the children
+        // if number of children is 2 then the angle increments should be 180deg
+        // if its 4 then it should be 90deg incrememnts
+        // once we know the angle we can choose a random distance from center to place the food
+        const increment = 2 * Math.PI / numChildren;
+        let angle = Math.random() * Math.PI; // choose random starting angle to provide some variation in placements
+        const maxDist = 200;
+        for(let i = 0 ; i < numChildren ; i++){
+            // if i know angle and distance then i know coordinates
+            const distance = Math.random() * maxDist;
+            const x = Math.cos(angle) * distance;
+            const y = Math.sin(angle) * distance;
+            this.game.entities.push(new Food(this.game, this.x + x, this.y + y, false));
+            angle+=increment;
+        }
     }
 
     update() {
@@ -65,13 +90,24 @@ class Food {
             setTimeout(() => {
                 this.state += 1;
             }, this.properties[this.state].lifeSpan * 1000);
+            if (this.state == this.states.adult) {
+                const maxChildCount = 3;
+                this.reproduce(Math.floor(Math.random() * (maxChildCount)) + 1);
+            }
         }
         this.updateBoundingCircle();
     }
 
     draw(ctx) {
         ctx.beginPath();
-        ctx.arc(this.x, this.y, this.properties[this.state].radius, 0 , 2 * Math.PI, false);
+        ctx.arc(
+            this.x,
+            this.y,
+            this.properties[this.state].radius,
+            0,
+            2 * Math.PI,
+            false
+        );
         ctx.fillStyle = this.properties[this.state].color;
         ctx.fill();
         ctx.lineWidth = 2;
